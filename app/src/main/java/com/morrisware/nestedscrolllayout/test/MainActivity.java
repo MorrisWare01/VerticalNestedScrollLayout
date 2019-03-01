5fb4b93529e99a2c9b1cd77d6f641801f3209d89
MorrisWare <MorrisWare01@gmail.com>
2019-03-01 18:26:09 +0800

    save

--- app/src/main/java/com/morrisware/nestedscrolllayout/test/MainActivity.java
+++ app/src/main/java/com/morrisware/nestedscrolllayout/test/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-//        mBinding.viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
-//        mBinding.tabLayout.setupWithViewPager(mBinding.viewPager);
+        mBinding.viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mBinding.tabLayout.setupWithViewPager(mBinding.viewPager);
     }
 
     private class MyPagerAdapter extends FragmentStatePagerAdapter {
